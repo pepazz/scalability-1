b80e95c0b72bbd5f33dbf0491410c3d404527177
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2023-05-17 14:46:50 -0300

    Create colored.py
    Função que retorna um texto colorido no print do colab

---
+++ general_utilities/colored.py
@@ -0,0 +1,13 @@
+# Função que retorna um texto colorido no print do colab
+
+def colored(texto,cor):
+  if cor == 'red' or cor == 'r':
+    return f'\033[1;31m{texto}\033[0;0;0m'
+  elif cor == 'green' or cor == 'g':
+    return f'\033[1;32m{texto}\033[0;0;0m'
+  elif cor == 'yellow' or cor == 'y':
+    return f'\033[1;33m{texto}\033[0;0;0m'
+  elif cor == 'blue' or cor == 'b':
+    return f'\033[1;34m{texto}\033[0;0;0m'
+  else:
+    return texto
