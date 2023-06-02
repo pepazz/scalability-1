7dee91611d618551585ffe51d32b9ba119fbcb15
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2023-06-02 19:23:10 -0300

    Update colored.py
    Modificando para imprimir colorido no DataBricks

--- general_utilities/colored.py
+++ general_utilities/colored.py
@@ -2,12 +2,16 @@
 
 def colored(texto,cor):
   if cor == 'red' or cor == 'r':
-    return f'\033[1;31m{texto}\033[0;0;0m'
+    #return f'\033[1;31m{texto}\033[0;0;0m'
+    return f'\x1b[31m{texto}\x1b[0m'
   elif cor == 'green' or cor == 'g':
-    return f'\033[1;32m{texto}\033[0;0;0m'
+    #return f'\033[1;32m{texto}\033[0;0;0m'
+    return f'\x1b[32m{texto}\x1b[0m'
   elif cor == 'yellow' or cor == 'y':
-    return f'\033[1;33m{texto}\033[0;0;0m'
+    #return f'\033[1;33m{texto}\033[0;0;0m'
+    return f'\x1b[33m{texto}\x1b[0m'
   elif cor == 'blue' or cor == 'b':
-    return f'\033[1;34m{texto}\033[0;0;0m'
+    #return f'\033[1;34m{texto}\033[0;0;0m'
+    return f'\x1b[34m{texto}\x1b[0m'
   else:
     return texto
