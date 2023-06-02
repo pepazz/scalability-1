e79fc0218aaa8239d92956c165a0ae3a3fa75cfa
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2023-06-02 14:24:51 -0300

    Update building_blocks.py

--- dynamic_cohort_funnel/building_blocks.py
+++ dynamic_cohort_funnel/building_blocks.py
@@ -2,7 +2,7 @@
 
 #Importando funções necessárias para rodar "building_blocks":
 import * from planning_cohort_funnel_auxiliary_functions
-import gerador_baseline_conversoes_v2 from gerador_baseline_conversoes_v2
+from gerador_baseline_conversoes_v2 import gerador_baseline_conversoes_v2
 
 
 def building_blocks(inputs_df,
