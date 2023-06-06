a1b8306b01976d900f491d5f61b68af2b5fbb580
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2023-06-06 11:25:43 -0300

    Update checks.py
    import pandas as pd

--- data_and_inputs_checks/checks.py
+++ data_and_inputs_checks/checks.py
@@ -3,6 +3,7 @@
 # Importando Bibliotecas necessárias:
 import numpy as np
 from colored import colored
+import pandas as pd
 
 def check_etapas_do_funil(lista_etapas_conversao, # lista com todas as etapas de conversão definidas pelo usuário no painel de controle
                           lista_topos_de_funil,   # lista com os ToF's definida pelo usuário no painel de controle
