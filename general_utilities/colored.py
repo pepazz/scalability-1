db08e64cfb9e8ec094d9894c5ba439a86edd491a
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2023-06-14 19:56:34 -0300

    teste 02

--- general_utilities/colored.py
+++ general_utilities/colored.py
@@ -5,8 +5,8 @@ def colored(texto,cor):
     #return f'\033[1;31m{texto}\033[0;0;0m'
     return f'\x1b[31m{texto}\x1b[0m'
   elif cor == 'green' or cor == 'g':
-    #return f'\033[1;31m{texto}\033[0;0;0m'
-    return f'\x1b[32m{texto}\x1b[0m'
+    #return f'\033[1;32m{texto}\033[0;0;0m'
+    return f'\x1b[31m{texto}\x1b[0m'
   elif cor == 'yellow' or cor == 'y':
     #return f'\033[1;33m{texto}\033[0;0;0m'
     return f'\x1b[33m{texto}\x1b[0m'
