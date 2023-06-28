7d16aa945a87c11f538bce91608ae5dc4f7497aa
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2023-06-28 17:07:03 -0300

    debug

--- data_and_inputs_checks/checks.py
+++ data_and_inputs_checks/checks.py
@@ -1004,7 +1004,7 @@ def check_colunas_bases_especificas(nome_do_arquivo,
   aberturas_lower = [e.lower() for e in aberturas]
   if 'região' in aberturas_lower and (len(df_city_share) == 0 or not flag_gerar_city_share):
     erro+=1
-    mensagem=mensagem+'\n\nNo arquivo '+colored(nomes_dos_arquivos[3],'blue')+' parece que não existe uma base de'+colored('City Share','yellow')+'. Esta base é obrigatória se houver uma abertura "região"'
+    mensagem=mensagem+'\n\nNo arquivo '+colored(nome_do_arquivo[3],'blue')+' parece que não existe uma base de'+colored('City Share','yellow')+'. Esta base é obrigatória se houver uma abertura "região"'
 
   if len(df_on_top_ratios) != 0:
     col_valores_on_top_originais = col_valores_on_top.copy()
