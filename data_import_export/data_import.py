e127dcbdb3d7aae61ec60e23245397220dd4f2b7
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2023-07-25 12:10:51 -0300

    Update data_import.py

--- data_import_export/data_import.py
+++ data_import_export/data_import.py
@@ -257,6 +257,7 @@ def abertura_das_bases(lista_de_nomes_das_bases,  # lista de strings com os nome
     else:
       print(colored(nome_da_base+' aberto',"green"),end="")
   else:
+    print(len(lista_de_bases),numero_de_bases)
     flag_abriu = False
   
   return lista_de_bases,flag_abriu
