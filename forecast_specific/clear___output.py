b31aaa862c514891e1d1cae6274adda2f3d862e9
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2024-01-19 10:28:51 -0300

    Update clear___output.py

--- forecast_specific/clear___output.py
+++ forecast_specific/clear___output.py
@@ -1,6 +1,5 @@
 #@title Definições de Manutenção
 from IPython.display import clear_output
-flag_clear = False
 def clear___output(flag):
   if flag:
-    clear_output(flag_clear)
+    clear_output(wait=True)
