375e5bbba5963e7624604f97113341e687e257ec
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2024-01-22 15:45:08 -0300

    Update algoritmo_genetico.py

--- forecast_specific/genetic_algorithm/algoritmo_genetico.py
+++ forecast_specific/genetic_algorithm/algoritmo_genetico.py
@@ -1,6 +1,8 @@
 #@title Def algoritmo_genetico (AG 3)
 from sklearn.linear_model import LinearRegression
 import numpy as np
+from fitness_func_qualidade_do_modelo import *
+from parametros_modelo import *
 
 def algoritmo_genetico(df_completo, # DF filtrado somente etapa e abertura e endógena e data, já com todas as combinações possíveis de exógenas (inclusive as endog transformadas)
                       col_exogs,   # Lista com o nome das colunas de exogs
