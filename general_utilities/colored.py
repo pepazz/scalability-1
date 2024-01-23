aa418d16eecd00914eda99910a492fe2a3eea53f
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2024-01-23 10:33:47 -0300

    Update colored.py

--- general_utilities/colored.py
+++ general_utilities/colored.py
@@ -4,7 +4,6 @@ def colored(texto,cor):
   if cor == 'red' or cor == 'r':
     #return f'\033[1;31m{texto}\033[0;0;0m'
     return f'\x1b[31m{texto}\x1b[0m'
-  '''
   elif cor == 'green' or cor == 'g':
     #return f'\033[1;32m{texto}\033[0;0;0m'
     return f'\x1b[32m{texto}\x1b[0m'
@@ -14,6 +13,5 @@ def colored(texto,cor):
   elif cor == 'blue' or cor == 'b':
     #return f'\033[1;34m{texto}\033[0;0;0m'
     return f'\x1b[34m{texto}\x1b[0m'
-  '''
   else:
     return texto
