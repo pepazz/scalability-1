3a75d159ef6f100bec30b437b8949c466014c4e4
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2024-01-23 10:23:43 -0300

    Update algoritmo_genetico.py

--- forecast_specific/genetic_algorithm/algoritmo_genetico.py
+++ forecast_specific/genetic_algorithm/algoritmo_genetico.py
@@ -129,7 +129,7 @@ def algoritmo_genetico(df_completo, # DF filtrado somente etapa e abertura e end
 
 
     # Aqui definimos o modelo do algoritmo genético
-    # teste
+    pygad = 'teste'
     ga_instance = pygad.GA(num_generations=num_generations,
                           num_parents_mating=num_parents_mating,
                           fitness_func=fitness_function,
