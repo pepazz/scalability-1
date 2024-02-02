9eb0a9a278973e5022499ac150b9231ae2487a8a
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2024-02-02 10:13:13 -0300

    Update clear___output.py
    False

--- forecast_specific/clear___output.py
+++ forecast_specific/clear___output.py
@@ -1,6 +1,6 @@
 #@title Definições de Manutenção
 from IPython.display import clear_output
-flag_clear = True # Apaga ou mantém todos os prints. Deixar como "False" quando quiser fazer manutenção do código e imprimir coisas internas
+flag_clear = False # Apaga ou mantém todos os prints. Deixar como "False" quando quiser fazer manutenção do código e imprimir coisas internas
 def clear___output(flag):
   if flag:
     clear_output(wait=True)
