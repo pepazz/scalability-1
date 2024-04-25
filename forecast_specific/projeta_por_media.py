c0b937b4bfd9e1797e0e50e15003b7ffad25ea6d
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2024-04-25 12:07:53 -0300

    Update projeta_por_media.py
    melhoria na projeção via média

--- forecast_specific/projeta_por_media.py
+++ forecast_specific/projeta_por_media.py
@@ -1,15 +1,39 @@
 #@title Def projeta_por_media
 import numpy as np
 # o df tem que estar ordenado por data com a mais antiga no topo
-def projeta_por_media(df,endogenous,qtd_semanas_projetadas,qtd_semanas_media):
+def projeta_por_media(df,
+                      endogenous,
+                      qtd_semanas_projetadas,
+                      qtd_semanas_media):
+  
+  if endogenous == '%__Volume Aberta':
+    serie_aberta = df['Volume Aberta'].astype('float').values
+    serie_volume = df['Volume'].astype('float').values
 
-  serie = df[endogenous].astype('float').values
+    if len(serie_aberta) >= qtd_semanas_media:
+      serie_aberta = serie_aberta[-qtd_semanas_media:]
+      serie_volume = serie_volume[-qtd_semanas_media:]
+    
+    avg = np.sum(serie_aberta)/np.sum(serie_volume)
+  
+  elif endogenous == 's__0':
+    serie_aberta = df['Volume Aberta'].astype('float').values
+    serie_0 = df['0'].astype('float').values    
 
+    if len(serie_aberta) >= qtd_semanas_media:
+      serie_aberta = serie_aberta[-qtd_semanas_media:]
+      serie_0 = serie_0[-qtd_semanas_media:]
 
-  if len(serie) >= qtd_semanas_media:
-    serie = serie[-qtd_semanas_media:]
+    avg = np.sum(serie_0)/np.sum(serie_aberta)
 
-  avg = np.mean(serie)
+  else:
+    serie = df[endogenous].astype('float').values
+
+    if len(serie) >= qtd_semanas_media:
+      serie = serie[-qtd_semanas_media:]
+
+    avg = np.mean(serie)
+
   forecast = np.zeros(shape=(int(qtd_semanas_projetadas)))
   forecast[:] = avg
 
