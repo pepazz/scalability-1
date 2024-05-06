a151549234816a9e032931428cb005acacf2b896
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2024-05-06 13:35:07 -0300

    Update base_de_inputs.py

--- forecast_specific/dynamic_cohort_funnel/base_de_inputs.py
+++ forecast_specific/dynamic_cohort_funnel/base_de_inputs.py
@@ -1,6 +1,7 @@
 #@title Def base_de_inputs
 from itertools import product
 import pandas as pd
+import numpy as np
 
 def base_de_inputs(df_inputs,  # DataFrame contendo os inputs
                    modelo,     # Só precisa ser um DataFrame contendo todos os ítens de todas as [aberturas + 'Week Origin']
