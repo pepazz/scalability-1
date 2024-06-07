9a0116cfa3288e812fb61df43d727c5930968dde
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2024-06-07 06:21:11 -0300

    Update forecast_2.py

--- forecast_specific/dynamic_cohort_funnel/forecast_2.py
+++ forecast_specific/dynamic_cohort_funnel/forecast_2.py
@@ -171,7 +171,7 @@ def forecast_2(df_completo,      # DataFrame filtrado, somente datas e valores
 
     # Realizamos a projeção
     if tipo_de_projecao != 'Cluster':
-      print("forecast_2----Auxiliar_Forecast---%0--------------------0")
+      print("forecast_2----Auxiliar_Forecast---s0--------------------1")
       print(df_completo_projetado[['week_start','Volume','Volume Aberta','0','%__Volume Aberta','%__0','s__0']])
       df_completo_projetado,out_parametros_c,matriz_forecast_c,base_outliers_local,base_contencao_de_danos_local =  Auxiliar_Forecast(df_completo = df_completo_projetado, # DF filtrado somente a abertura e Endog, ordenado com a data mais antiga no topo
                                                                                                                                       df_inputs_exogenas = df_inputs_exogenas,
@@ -198,7 +198,7 @@ def forecast_2(df_completo,      # DataFrame filtrado, somente datas e valores
                                                                                                                                         limite_delta_aberta = limite_delta_aberta,
                                                                                                                                       fit_intercept = fit_intercept,
                                                                                                                                      limite_proj = limite_proj)
-      print("forecast_2----Auxiliar_Forecast---s0--------------------1")
+      print("forecast_2----Auxiliar_Forecast---s0--------------------2")
       print(df_completo_projetado[['week_start','Volume','Volume Aberta','0','%__Volume Aberta','%__0','s__0']])
       
       # Atualizamos as bases periféricas:
@@ -518,7 +518,7 @@ def forecast_2(df_completo,      # DataFrame filtrado, somente datas e valores
 
   # Realizamos a projeção
   if tipo_de_projecao != 'Cluster':
-    print("forecast_2----Auxiliar_Forecast---'%__Volume Aberta'--------------------0")
+    print("forecast_2----Auxiliar_Forecast---'%__Volume Aberta'--------------------1")
     print(df_completo_projetado[['week_start','Volume','Volume Aberta','0','%__Volume Aberta','%__0']])
     df_completo_projetado,out_parametros_a,matriz_forecast_a,base_outliers_local,base_contencao_de_danos_local =  Auxiliar_Forecast(df_completo = df_completo_projetado, # DF filtrado somente a abertura e Endog, ordenado com a data mais antiga no topo
                                                                                                                                     df_inputs_exogenas = df_inputs_exogenas,
@@ -546,7 +546,7 @@ def forecast_2(df_completo,      # DataFrame filtrado, somente datas e valores
                                                                                                                                     fit_intercept = fit_intercept,
                                                                                                                                    limite_proj = limite_proj)
 
-    print("forecast_2----Auxiliar_Forecast---'%__Volume Aberta'--------------------1")
+    print("forecast_2----Auxiliar_Forecast---'%__Volume Aberta'--------------------2")
     print(df_completo_projetado[['week_start','Volume','Volume Aberta','0','%__Volume Aberta','%__0']])
     # Atualizamos as bases periféricas:
     base_contencao_de_danos = pd.concat([base_contencao_de_danos,base_contencao_de_danos_local])
@@ -663,7 +663,7 @@ def forecast_2(df_completo,      # DataFrame filtrado, somente datas e valores
       np.transpose(np.array([df_completo_projetado.loc[df_completo_projetado[col_data] >= primeira_data_forecast, cohorts_projetadas[0:i-1]].sum(axis=1).values])))
     '''
   print("forecast_2----output--------------------0")
-  print(df_completo_projetado)
+  print(df_completo_projetado[['week_start','Volume','Volume Aberta','0','%__Volume Aberta','%__0']])
   #if tipo_de_projecao == 'Cluster':
     #print(df_completo_projetado[['Etapa','Volume','%__Volume Aberta','0']])
   return df_completo_projetado,out_parametros,matriz_forecast,base_outliers,base_contencao_de_danos
