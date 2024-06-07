36e2b86ef41c8b5d1d745794530f72c3b34d9195
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2024-06-07 06:48:01 -0300

    Update aplica_forecast.py

--- forecast_specific/dynamic_cohort_funnel/aplica_forecast.py
+++ forecast_specific/dynamic_cohort_funnel/aplica_forecast.py
@@ -232,8 +232,7 @@ def aplica_forecast(df_completo,      # DataFrame completo filtrado na etapa
 
       # Realizamos a projeção de todas as variáveis da abertura (Volumes de topo, shares de cohorts, cohort aberta e cohorts fechadas)
       #print(df_completo_abertura[[col_data,'Volume Aberta','%__Volume Aberta','1','s__1','%__1']])
-      print("aplica_forecast--------------------------------------1")
-      print(df_completo_abertura[['week_start','Volume','Volume Aberta','0','%__Volume Aberta','%__0']])
+
       df_completo_abertura,out_parametros_f,matriz_forecast_f,base_outliers_local,base_contencao_de_danos_local = forecast_2(df_completo = df_completo_abertura,      # DataFrame filtrado, somente datas e valores
                                                                                                                             df_parametros = df_parametros_abertura,
                                                                                                                             df_pareto_aberturas = df_pareto_aberturas_abertura,
@@ -264,8 +263,7 @@ def aplica_forecast(df_completo,      # DataFrame completo filtrado na etapa
                                                                                                                               limite_delta_aberta = limite_delta_aberta,
                                                                                                                               limite_proj = limite_proj,
                                                                                                                              fit_intercept = fit_intercept)
-      print("aplica_forecast--------------------------------------2")
-      print(df_completo_abertura[['week_start','Volume','Volume Aberta','0','%__Volume Aberta','%__0']])
+
       # Salvamos os outliers encontrados numa base:
       base_outliers = pd.concat([base_outliers,base_outliers_local])
 
